01a5e151f26f305e0973f041fc69dd54de32a1aa
VariianWrynn <778370722@qq.com>
2025-01-10 11:44:28 +0800

    test_zephyr wq!

--- test_zephyr.py
+++ test_zephyr.py
@@ -1,22 +1,11 @@
-import requests
-
-url = "http://localhost:11434/api/v1/completions"
-headers = {"Content-Type": "application/json"}
-data = {
-    "model": "zephyr-7b-alpha",
-    "prompt": "Hello, how are you?"
-}
-
-response = requests.post(url, json=data, headers=headers)
-
-# 检查响应状态码
-if response.status_code == 200:
-    try:
-        json_response = response.json()
-        print(json_response.get("completion", "No completion key found"))
-    except requests.exceptions.JSONDecodeError as e:
-        print("JSON Decode Error:", e)
-        print("Raw Response Text:", response.text)
-else:
-    print("Error:", response.status_code)
-    print("Raw Response Text:", response.text)
+from ollama import Client
+client = Client(
+    host='http://192.168.11.69:11434',
+    headers = {"Content-Type": "application/json"}
+)
+response = client.chat(model='zephyr', messages=[
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
+])
